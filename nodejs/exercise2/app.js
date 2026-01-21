166f0b3a5232de103e6c7a77da08400966647a78
AryanYadav888 <aryanceo888@gmail.com>
2026-01-21 18:16:46 +0530

    Delete nodejs directory

--- nodejs/exercise2/app.js
+++
@@ -1,9 +0,0 @@
-const su = require('./stringUtils');
-
-const text = "Hello, World!";
-
-console.log("Original : "+text);
-console.log("Uppercase: "+su.capitalize(text));
-console.log("reversed: "+su.reverseString(text));
-console.log("Palindrome: "+su.isPalindrome(text));
-console.log("Vowels   : "+su.countVowels(text));
