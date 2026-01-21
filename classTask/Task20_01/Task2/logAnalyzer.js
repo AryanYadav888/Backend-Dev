3bf4d6f196042894bbf3ea0bbb95da14e37e8bfe
blacksmith011 <aryanceo888@gmail.com>
2026-01-21 19:47:31 +0530

    Add Task log file analyzer using streams

---
+++ classTask/Task20_01/Task2/logAnalyzer.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const logfilePath = "server.log";
+const reportfilePath = "summary.txt";
+
+let totallines = 0;
+let errorCount = 0;
+let warningCount = 0;
+let infoCount = 0;
+
+const readStream = fs.createReadStream(logfilePath,{
+    encoding : "utf-8"
+});
+
+const r1 = readline.createInterface({
+    input : readStream,
+    crlfDelay : Infinity
+});
+
+r1.on("line",(line) => {
+    totallines++;
+
+    if(line.includes("ERROR")) errorCount++;
+    else if(line.includes("WARNING")) warningCount++;
+    else if(line.includes("INFO")) infoCount++;
+});
+
+r1.on("close" ,() => {
+    const report = `
+    Log File Analysis
+    total Line : ${totallines}
+    error count : ${errorCount}
+    warning count : ${warningCount}
+    info count : ${infoCount}
+    `;
+
+    const writeStream = fs.createWriteStream(reportfilePath);
+    writeStream.write(report);
+    writeStream.end();
+
+    console.log("Done");
+    console.log("Summary");
+});
+
+readStream.on("error",(err) => {
+    console.log("Errrrr" , err.message);
+})
